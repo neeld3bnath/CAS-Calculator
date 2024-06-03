0def39e3a8a156029791cb922d4e4cc0fca8f4d2
ninjastar779 <neeldebnath2@gmail.com>
2024-06-03 19:41:44 -0400

    Added more graphing options

--- beginner.py
+++ beginner.py
@@ -1,6 +1,5 @@
 from sympy import *
-import matplotlib.pyplot as plt
-import numpy as np
+from sympy import simplify
 
 init_printing()
 a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z = symbols('a b c d e f g h i j k l m n o p q r s t u v w x y z')
@@ -9,7 +8,7 @@ print("Welcome to my algebra calculator!")
 while True:
     print("What do you want to do?")
 
-    print("1. Solve for a variable")
+    print("1. Solve for a variable in an equation")
     print("2. Find the derivative of a function")
     print("3. Find the integral of a function")
     print("4. Find the limit of a function")
@@ -17,11 +16,13 @@ while True:
     print("6. Solve a system of equations")
     print("7. Find the y-value of a function at an x value")
     print("8. Rationalize the denominator")
-    print("9. Graph a function")
-    print("10. Factor an expression")
-    print("11. Find the exact value of a trig function (may return something useless)")
-    print("12. Solve an inequality")
-    print("13. Quit")
+    print("9. Graph a rectangular function")
+    print("10. Graph a parametric function")
+    print("11. Factor an expression")
+    print("12. Find the exact value of a trig function (may return something useless)")
+    print("13. Solve an inequality")
+    print("14. Evaluate an expression")
+    print("15. Quit")
 
     user = int(input("Enter your choice: "))
     if user == 1:
@@ -31,83 +32,97 @@ while True:
         secondhalf = input("Enter the second part of the equation (after the equals sign): ")
         secondhalf = parse_expr(secondhalf)
         equation = Eq(firsthalf,secondhalf)
-        pprint(solve(equation))
-
+        symbol = input("Enter the variable you like to solve for: ")
+        pprint(solve(equation,symbol))
     if user == 2:
         print()
-        input2 = input("Enter the function: ")
-        input2 = parse_expr(input2)
-        pprint(diff(input2))
+        function = input("Enter the function: ")
+        function = parse_expr(function)
+        pprint(diff(function))
     if user == 3:
         print()
-        input2 = input("Enter the function: ")
-        input2 = parse_expr(input2)
-        input3 = input("Enter the variable: ")
-        pprint((Integral(input2),x))
+        function = input("Enter the function: ")
+        function = parse_expr(function)
+        symbol = input("Enter the variable: ")
+        pprint((Integral(function),symbol))
     if user == 4:
         print()
-        input2 = input("Enter the function: ")
-        input2 = parse_expr(input2)
-        input3 = input("From the left or right? (l/r): ")
-        if input3 == "l":
-            pprint(limit(input2,x,-oo))
-        if input3 == "r":
-            pprint(limit(input2,x,oo))
+        func = input("Enter the function: ")
+        func = parse_expr(func)
+        symbol = input("Enter the variable: ")
+        lim = input("From the left or right? (l/r): ")
+        if lim == "l":
+            pprint(limit(func,symbol,-oo))
+        if lim == "r":
+            pprint(limit(func,symbol,oo))
     if user == 5:
         print()
-        input2 = input("Enter the expression: ")
-        input2 = parse_expr(input2)
-        input3 = input("Enter the variable: ")
-        input3 = parse_expr(input3)
-        pprint(roots(input2,input3))
+        expression = input("Enter the expression: ")
+        expression = parse_expr(expression)
+        symbol = input("Enter the variable: ")
+        symbol = parse_expr(symbol)
+        pprint(roots(expression,symbol))
     if user == 6:
         print()
         fhalf1 = input("Enter the first half of the first equation: ")
         fhalf1 = parse_expr(fhalf1)
-        fhalf2 = input("Enter the second half of the first equation: ")
-        fhalf2 = parse_expr(fhalf2)
-        shalf1 = input("Enter the first half of the second equation: ")
+        shalf1 = input("Enter the second half of the first equation: ")
         shalf1 = parse_expr(shalf1)
+        fhalf2 = input("Enter the first half of the second equation: ")
+        fhalf2 = parse_expr(fhalf2)
         shalf2 = input("Enter the second half of the second equation: ")
         shalf2 = parse_expr(shalf2)
-        equation1 = Eq(fhalf1,fhalf2)
-        equation2 = Eq(shalf1,shalf2)
+        equation1 = Eq(fhalf1,shalf1)
+        equation2 = Eq(fhalf2,shalf2)
         pprint(solve([equation1,equation2]))
     if user == 7:
         print()
-        input2 = input("Enter the function: ")
-        input2 = parse_expr(input2)
+        func = input("Enter the function: ")
+        func = parse_expr(func)
         value = input("Enter the value of x: ")
-        pprint(input2.subs(x,value))
+        pprint(func.subs(x,value))
     if user == 8:
         print()
-        input2 = input("Enter the numerator: ")
-        input2 = parse_expr(input2)
-        input3 = input("Enter the denominator (use sqrt() for square root): ")
-        input3 = parse_expr(input3)
-        pprint(simplify(input2/input3))
+        numerator = input("Enter the numerator: ")
+        numerator = parse_expr(numerator)
+        denominator = input("Enter the denominator (use sqrt() for square root if needed): ")
+        denominator = parse_expr(denominator)
+        pprint(simplify(numerator/denominator))
     if user == 9:
         print()
-        input2 = input("Enter the function to graph: ")
-        input2 = parse_expr(input2)
+        func = input("Enter the function of x: ")
+        func = parse_expr(func)
         boundary = input("Do you want to set boundaries on the domain? (y/n): ")
         if boundary == "y":
             lower = float(input("Enter the lower bound: "))
             upper = float(input("Enter the upper bound: "))
-            p = plot(input2, (x,lower,upper))
+            plot(func, (x, lower, upper), title=r"$%s$" %latex(func))
         elif boundary == "n":
-            p = plot(input2)
+            plot(func, (x, -10, 10), adaptive=True, title=r"$%s$" %latex(func))
     if user == 10:
         print()
-        input2 = input("Enter the expression: ")
-        input2 = parse_expr(input2)
-        pprint(factor(input2))
+        funcx = input("Enter the x-function (make u the variable): ")
+        funcx = parse_expr(funcx)
+        funcy = input("Enter the y-function (make u the variable): ")
+        funcy = parse_expr(funcy)
+        boundary = input("Do you want to set boundaries on the domain? (y/n): ")
+        if boundary == "y":
+            lower = float(input("Enter the lower bound: "))
+            upper = float(input("Enter the upper bound: "))
+            plot_parametric((funcx, funcy), (u, lower, upper))
+        elif boundary == "n":
+            plot_parametric((funcx, funcy), (u, -10, 10), adaptive=True)
     if user == 11:
         print()
-        input2 = input("What trig function do you want to find the exact value of? (sin, cos, tan, cot, sec, csc): ")
+        expression = input("Enter the expression: ")
+        expression = parse_expr(expression)
+        pprint(factor(expression))
+    if user == 12:
+        print()
+        trig = input("What trig function do you want to find the exact value of? (sin, cos, tan, cot, sec, csc): ")
         print("\nYou can use 'sqrt()' for square root or 'pi' for pi.")
         trig_functions = {"sin": sin, "cos": cos, "tan": tan, "cot": cot, "sec": sec, "csc": csc}
-        trig_function = trig_functions.get(input2)
+        trig_function = trig_functions.get(trig)
         if trig_function:
             input3 = input("Enter the value (must be in radians): ")
             answer = trig_function(parse_expr(input3))
@@ -115,14 +130,21 @@ while True:
                 print("Value is undefined.")
             else:
                 pprint(answer)
-    if user == 12:
+        else:
+            print("Invalid input.")
+    if user == 13:
         print()
         print("You can use >, >=, <, and <=.")
-        input2 = input("Enter the inequality: ")
-        input2 = parse_expr(input2)
-        input3 = input("Enter the variable: ")
-        input3 = parse_expr(input3)
-        pprint(reduce_inequalities(input2,input3))
-    if user == 13:
+        inequality = input("Enter the inequality: ")
+        inequality = parse_expr(inequality)
+        symbol = input("Enter the variable: ")
+        symbol = parse_expr(symbol)
+        pprint(reduce_inequalities(inequality,symbol))
+    if user == 14:
+        print()
+        expression = input("Enter the expression: ")
+        expression = parse_expr(expression)
+        pprint(simplify(expression))
+    if user == 15:
         print("Goodbye!")
         break
